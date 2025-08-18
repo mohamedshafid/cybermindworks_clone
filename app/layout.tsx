87838c69ed20a0eda29d79dd28bea6469964dd5c
Mohamed Hafid <mohamedhafid825@gmail.com>
2025-08-18 19:34:21 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "CyberMind Works | Mohamed Hafid",
+  description:
+    "A Full-stack Admin Job Panel built with Next.js and TypeScript",
+};
+
+const satoshi = localFont({
+  src: [
+    {
+      path: "../assets/fonts/Satoshi-Variable.ttf",
+      style: "normal",
+    },
+  ],
+  variable: "--font-satoshi",
+  display: "swap",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${satoshi.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='underline text-4xl font-satoshi'>
+      This is Mohamed Hafid
+    </div>
+  )
+}
+
+export default Home
